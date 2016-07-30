1c47db199b1f7014fe9a6855e5dc2a72c1da0665
shivamswarnkar <ss8464@nyu.edu>
2016-07-30 23:53:09 +0530

    added game's layout

--- game.js
+++ game.js
@@ -3,3 +3,30 @@
  *situation of the game.
 */ 
 
+function mark(curr, user){
+	if(user == null){
+		curr.innerHTML = "X";
+		curr.style.color = "green";
+		curr.value = "disable";
+		deactive(true);
+	}
+
+}
+
+function deactive(bool){
+	var places = document.getElementsByClassName("place");
+	for (var i=0;  i < places.length; ++i){
+		places[i].disabled = bool;
+	}
+	
+}
+
+
+
+/*------AI--------*/
+
+
+
+
+
+/*------AI--------*/
